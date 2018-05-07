2bdbc376ad099f24491df290ddf6c60c0c6760cc
Javier Cabero <cabero@Javiers-MacBook-Pro.local>
2018-05-07 22:34:44 +0200

    refactoring

--- flask-classifier-api/server.py
+++ flask-classifier-api/server.py
@@ -1,19 +1,23 @@
+###########################################
+# Copyright (c) Javier Cabero Guerra 2018 #
+# Licensed under MIT                      #
+###########################################
+
+
 import os
+import random
+from flask import Flask
+from flask_restful import Resource, Api
 
-def run_api():
-  import random
-  from flask import Flask
-  from flask_restful import Resource, Api
-  app = Flask(__name__, static_url_path='/static')
-  api = Api(app)
-  
-  port = int(os.getenv('PORT', 8000)) 
-  class APITest(Resource):
-    def get(self):
-    	return {'test':  random.randint(1, 10)}
+app = Flask(__name__)
+api = Api(app)
 
-  api.add_resource(APITest, '/') 
+port = int(os.getenv('PORT', 8000)) 
 
-  app.run(host='0.0.0.0', port=port)
+class APITest(Resource):
+  def get(self):
+  	return {'test':  random.randint(1, 10)}
 
-run_api()
+api.add_resource(APITest, '/') 
+
+app.run(host='0.0.0.0', port=port)
