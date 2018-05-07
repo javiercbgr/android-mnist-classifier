a83e128aec7a6ab21ea9178f77694f6fccdfb987
Javier Cabero <cabero@Javiers-MacBook-Pro.local>
2018-05-07 22:57:44 +0200

    get parameter working

--- flask-classifier-api/server.py
+++ flask-classifier-api/server.py
@@ -7,7 +7,7 @@
 import os
 import random
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 
 app = Flask(__name__)
 api = Api(app)
@@ -16,7 +16,11 @@ port = int(os.getenv('PORT', 8000))
 
 class APITest(Resource):
   def get(self):
-  	return {'test':  random.randint(1, 10)}
+  	#return {'test':  random.randint(1, 10)}
+    parser = reqparse.RequestParser()
+    parser.add_argument('foo', type=str)
+
+    return parser.parse_args()
 
 api.add_resource(APITest, '/') 
 
